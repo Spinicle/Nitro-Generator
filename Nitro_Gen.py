713a046c24d009647a704ee4250cf1fefae158d5
Spinicle <70312979+Spinicle@users.noreply.github.com>
2021-02-09 14:51:43 +0530

    Add files via upload

---
+++ Nitro_Gen.py
@@ -0,0 +1,17 @@
+import random, string
+import time
+
+num=input('Input How Many Codes to Generate: ')
+
+f=open("Nitro Codes.txt","w", encoding='utf-8')
+
+print("Generating...")
+      
+for n in range(int(num)):
+   y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
+   f.write('https://discord.gift/')
+   f.write(y)
+   f.write("\n")
+
+f.close()
+input("\n\nFinished, Press enter to exit. Codes saved in Nitro Codes")
